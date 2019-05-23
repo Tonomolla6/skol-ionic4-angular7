c9884fcbfe1ad77c48408b00dbc559c1e56efc65
tonomolla6 <tonomollag6@gmail.com>
2019-05-23 19:53:36 +0200

    Tono -> Testendo con typescript

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -10,12 +11,39 @@ import { Storage } from '@ionic/storage';
 })
 
 export class HomePage {
-  constructor(public router: Router, private storage: Storage) {}
-  user1: string;
-  user2: string;
-  user3: string;
+  users: object[];
+  posicion: string;
+
+  constructor(public router: Router, private storage: Storage, public activeRoute:ActivatedRoute) {
+    this.users = [
+      {name:'Jugador 1'},
+      {name:'Jugador 2'},
+      {name:'Jugador 3'}
+    ]
+    localStorage.clear();
+  }
+  
+
   onClick() {
-    localStorage.setItem('user1', this.user1);
-    this.router.navigate (['questions']);
+    //this.users.forEach(function(usuario) {
+      //localStorage.setItem("usuarios", JSON.stringify(this.activeRoute.snapshot.paramMap.get(`${usuario["name"]}`)));
+      //console.log(this.activeRoute.snapshot.paramMap.get(`${usuario["name"]}`));
+    //});
+
+    console.log(this.activeRoute.snapshot.paramMap.get('Jugador 1'));
+
+    ///
+    //let users = localStorage.getItem('usuarios')
+    //let randomNumber = 2;
+    //let user = null;
+    //user = users[randomNumber];
+
+    // /localStorage.setItem("usuarios", JSON.stringify(this.users));
+
+  //this.router.navigate (['questions']);
+  }
+  mas_jugadores(){
+    let totalUser = this.users.length + 1;
+    this.users.push({name: `Jugador ${totalUser}`});
   }
 }
