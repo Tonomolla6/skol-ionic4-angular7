00b10928b59b84535c3e028a4d9689248b4076a6
tonomolla6 <tonomollag6@gmail.com>
2019-05-27 09:11:52 +0200

    Tono -> Creando vista siguente

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,27 +9,85 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class HomePage {
-  users: object[];
   constructor(public router: Router, public activeRoute:ActivatedRoute) {
     this.users = [
       {name:'Jugador 1', value:''},
       {name:'Jugador 2', value:''},
       {name:'Jugador 3', value:''}
     ]
+    this.cartas = ["Fiesta", "Coma Etílico", "Futbol", "Famosos"];
+    this.h2_posicion = 0;
   }
+  /* ESTA PARTE ES PARA LA VISTA 1 */
+    users: object[];
+    
+    boton_vista_uno() {
+      //this.router.navigate (['questions'])
+      console.log(this.users);
+      document.getElementById("partesuperior").style.opacity = "0";
+      document.getElementById("barra").style.transform = "translateY(-83px)";
+      document.getElementById("atras_vista_uno").style.opacity = "1";
+      document.getElementById("silla2").style.transform = "translateX(-80px)";
+      document.getElementById("silla3").style.transform = "translateX(80px)";
+      document.getElementById("silla1").style.opacity = "0";
+      document.getElementById("silla4").style.opacity = "0";
+      document.getElementById("carta_titulo").style.opacity = "1";
+      document.getElementById("boton_vista_uno").style.opacity = "0";
+      document.getElementById("boton_vista_dos").style.opacity = "1";
+      document.getElementById("boton_vista_dos").style.zIndex = "3";
+      document.getElementById("formulario_usuarios").style.opacity = "0";
+      document.getElementById("formulario_cartas").style.display = "flex";
+      setTimeout(function(){
+        document.getElementById("formulario_usuarios").style.display = "none";
+        document.getElementById("formulario_cartas").style.opacity = "1";
+      }, 200);
+    }
 
-  //user = this.activeRoute.snapshot.paramMap.get('user1');
+    mas_jugadores() {
+      let totalUser = this.users.length + 1;
+      this.users.push({name: `Jugador ${totalUser}`, value:''});
+    }
 
-  x = 3;
+    /* ESTA PARTE ES PARA LA VISTA DOS */
+    cartas: string[];
+    h2_posicion: number;
 
-  onClick() {
-    //this.router.navigate (['questions'])
-    console.log(this.users);
-  }
+    atras_vista_uno() {
+      document.getElementById("partesuperior").style.opacity = "1";
+      document.getElementById("barra").style.transform = "translateY(0px)";
+      document.getElementById("atras_vista_uno").style.opacity = "0";
+      document.getElementById("silla2").style.transform = "translateX(0px)";
+      document.getElementById("silla3").style.transform = "translateX(0px)";
+      document.getElementById("silla1").style.opacity = "1";
+      document.getElementById("silla4").style.opacity = "1";
+      document.getElementById("carta_titulo").style.opacity = "0";
+      document.getElementById("boton_vista_uno").style.opacity = "1";
+      document.getElementById("boton_vista_dos").style.opacity = "0";
+      document.getElementById("boton_vista_dos").style.zIndex = "0";
+      document.getElementById("formulario_cartas").style.opacity = "0";
+      
+      setTimeout(function(){
+        document.getElementById("formulario_usuarios").style.display = "block";
+        document.getElementById("formulario_cartas").style.display = "none";
+        document.getElementById("formulario_usuarios").style.opacity = "1";
+      }, 200);
+    }
 
-  mas_jugadores() {
-    let totalUser = this.users.length + 1;
-    this.users.push({name: `Jugador ${totalUser}`, value:''});
-  }
+      posicion_cartas(direccion) {
+        if (direccion == "derecha") {
+          if (this.h2_posicion == this.cartas.length-1) {
+            this.h2_posicion = 0;
+          } else {
+            this.h2_posicion = this.h2_posicion + 1;
+          }
+        } else if (direccion == "izquierda") {
+          if (this.h2_posicion == 0) {
+            this.h2_posicion = this.cartas.length-1;
+          } else {
+            this.h2_posicion = this.h2_posicion - 1;
+          }
+        }
+        document.getElementById("titulo_carta").innerHTML = this.cartas[this.h2_posicion];
+      }
 }
 
